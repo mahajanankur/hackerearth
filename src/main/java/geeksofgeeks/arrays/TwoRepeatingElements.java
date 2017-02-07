d9b096a10889b1641d59546ed4923b22c1872946
Ankur Mahajan <mahajanankur.nith@gmail.com>
2017-02-08 00:26:18 +0530

    added more

---
+++ src/main/java/geeksofgeeks/arrays/TwoRepeatingElements.java
@@ -0,0 +1,21 @@
+package geeksofgeeks.arrays;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TwoRepeatingElements {
+
+	public static void main(String[] args) {
+		int arr[] = new int[] { 1, 2, 3, 4, 1, 2, 3, 4 };
+		fistRepeatingElemet(arr);
+	}
+
+	private static void fistRepeatingElemet(int[] arr) {
+		Map<Integer, Integer> count = new LinkedHashMap<>();
+		for (int i = 0; i <= arr.length; i++) {
+			
+		}
+
+	}
+
+}
