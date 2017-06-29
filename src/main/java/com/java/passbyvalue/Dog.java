fd3a61b2544ad1e51493a7b8fd54dc64e871cbd4
Ankur Mahajan <mahajanankur.nith@gmail.com>
2017-06-29 11:55:10 +0530

    Pass by value

---
+++ src/main/java/com/java/passbyvalue/Dog.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.java.passbyvalue;
+
+/**
+ * @author amahajan
+ *
+ * @written 28-Jun-2017
+ */
+public class Dog {
+
+	private String name;
+
+	public Dog(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/com/java/passbyvalue/TestPassByValue.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.java.passbyvalue;
+
+/**
+ * @author amahajan
+ *
+ * @written 28-Jun-2017
+ */
+public class TestPassByValue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// check pass by value and the value should not be changed.
+		Dog dog = new Dog("sultaan");
+		checkPassByValue(dog);
+		System.out.println("Dog Name after MAIN() method return : " + dog.getName());
+
+		// Check how to change value.
+		changeValueOfObject(dog);
+		System.out.println("Dog Name after changeValueOfObject() method return : " + dog.getName());
+
+	}
+
+	private static void changeValueOfObject(Dog dog) {
+		System.out.println("Dog Name in checkPassByValue() : " + dog.getName());
+		dog.setName("boxer");
+
+	}
+
+	private static void checkPassByValue(Dog dog) {
+		System.out.println("Dog Name in checkPassByValue() : " + dog.getName());
+		dog = new Dog("boxer");
+		System.out.println("Dog Name in checkPassByValue() after changing value : " + dog.getName());
+	}
+
+}
