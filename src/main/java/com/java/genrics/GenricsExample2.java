adfe51c6024ccc2bab1c37feb422324e2789e10f
ankur.mahajan <ankur.mahajan@in.g4s.com>
2019-02-15 17:31:08 +0530

    Genrics addition

--- src/main/java/com/java/genrics/GenricsExample2.java
+++ src/main/java/com/java/genrics/GenricsExample2.java
@@ -9,7 +9,7 @@ import com.java.collection.comparator.Person2;
  * @author amahajan
  *
  */
-public class GenricsExample2 {
+public class GenricsExample2<E> {
 
 	public <A extends Person2> A get(String str) {
 		System.out.println("in Generic");
@@ -18,9 +18,14 @@ public class GenricsExample2 {
 		return a;
 
 	}
-	
+
 	public static <T extends Person2> T firstGenericTest(T a) {
-		
+
 		return (T) new Person2("Ankur", "mahajan", "454545");
+	}
+
+	public E firstGenericTest(String a) {
+
+		return (E) new Person2("Ankur", "mahajan", "454545");
 	}
 }
