1763341e306506c49dff9568e3fb8e7800e165b8
Ankur Mahajan <mahajanankur.nith@gmail.com>
2020-09-10 23:34:04 +0530

    Added random string generation by common lang3

---
+++ src/main/java/com/common/lang3/GenerateRandomString.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.common.lang3;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+/**
+ * @author ankur
+ *
+ */
+public class GenerateRandomString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String value = RandomStringUtils.randomAlphabetic(10);
+		System.out.println(value);
+			 
+	
+	}
+
+}
